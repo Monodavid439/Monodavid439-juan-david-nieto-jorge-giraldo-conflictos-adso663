fc685f3a997bad3b6aaaea5cc221dc9e4ee6bbc7
Juan David <junieto.975@gmail.com>
2025-03-19 20:27:27 -0500

    se creo la funcion calculateAge juan david nieto

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 function saludar(){
-    
+
+}
+
+function calculateAge(age){
+    if(age >= 18){
+        console.log("Eres mayor de eda"+ age);
+    }else{
+        console.log("Eres menor de edad"+ age);
+    }
 }
